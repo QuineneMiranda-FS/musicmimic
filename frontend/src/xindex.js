b6c324fba9b956a130370364688d41a817e0cb52
Miranda Quinene <mequinene@student.fullsail.edu>
2026-06-04 17:09:29 -0400

    file cleanup frontend

--- frontend/src/xindex.js
+++
@@ -1,8 +0,0 @@
-// Import app.js
-const app = require("./xapp");
-
-// Set app to listen on port 3001
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Music Mimic running on port ${PORT}`);
-});
