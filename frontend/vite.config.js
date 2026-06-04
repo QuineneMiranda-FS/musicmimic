a8595b2b80e30f3711bad8e7877abea2ba6517bd
Miranda Quinene <mequinene@student.fullsail.edu>
2026-06-04 16:04:40 -0400

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/vue";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      // Allows '@' shortcut for src imports
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 5173, // Standard Vite dev port
+    proxy: {
+      // Any request starting with /api sent to Express
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
